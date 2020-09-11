144ea86428e2adbbdc4532abfd0cd23861d7aaff
Rezouce <contact@rezouce.net>
2020-09-11 10:41:31 +0200

    Basic implementation of the Hashmap using a PHP array to ensure the tests pass.

--- src/Hashmap.php
+++ src/Hashmap.php
@@ -4,4 +4,26 @@ namespace Easyrecrue;
 
 class Hashmap
 {
+    private string $functionNameForKeyHash;
+
+    private array $data = [];
+
+    public function __construct(string $functionNameForKeyHash)
+    {
+        $this->functionNameForKeyHash = $functionNameForKeyHash;
+    }
+
+    public function add($value): self
+    {
+        $function = $this->functionNameForKeyHash;
+
+        $this->data[$function($value)] = $value;
+
+        return $this;
+    }
+
+    public function find(string $key)
+    {
+        return $this->data[$key];
+    }
 }
